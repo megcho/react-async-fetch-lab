765a60b3e9359ef7ee750562d61436422cf16b28
megan cho <meganlisacho@gmail.com>
2021-07-02 21:53:54 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 // create your App component here
+import React from 'react';
+
+export default class App extends React.Component {
+
+    constructor() {
+        super()
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json');
+    }
+
+    render() {
+        return "Hi";
+    }
+}
